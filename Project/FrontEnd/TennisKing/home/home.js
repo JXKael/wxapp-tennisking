05c61a12be6ae2f9c701e95227f12f61bdfc65fb
jinxiangkai <jinxiangkai@foxmail.com>
2018-10-07 23:00:22 +0800

    首页

---
+++ Project/FrontEnd/TennisKing/home/home.js
@@ -0,0 +1,100 @@
+// pages/home/home.js
+
+var menu_data = require("../../utils/menus.js")
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    txt_tab_news: "赛事新闻",
+    txt_tab_players: "选手资讯",
+    currTabID: 0,
+    scroll_menu: [],
+    currMenuID: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      currTabID: 0,
+      scroll_menu: menu_data.news
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {},
+
+  /**
+   * 点击赛事新闻
+   */
+  onTapNews: function (e) {
+    console.log("点击赛事新闻")
+    this.setData({
+      currTabID: 0,
+      scroll_menu: menu_data.news
+    })
+  },
+
+  /**
+   * 点击选手资讯
+   */
+  onTapPlayers: function (e) {
+    console.log("点击选手资讯")
+    this.setData({
+      currTabID: 1,
+      scroll_menu: menu_data.players
+    })
+  },
+
+  /**
+   * 用户点击菜单事件
+   */
+  onTapMenuItem: function (e) {
+    console.log("用户点击类目")
+    console.log(e)
+    var tapID = e.currentTarget.dataset.index
+    var menu = this.data.scroll_menu
+    menu[this.data.currMenuID].choosed = false
+    menu[tapID].choosed = true
+    this.setData({
+      scroll_menu: menu,
+      currMenuID: tapID
+    })
+  }
+})
---
+++ Project/FrontEnd/TennisKing/menus.js
@@ -0,0 +1,49 @@
+const news_menu = [
+  { id: 0, name: "全部比赛", choosed: true },
+  { id: 1, name: "ATP", choosed: false },
+  { id: 2, name: "WTA", choosed: false },
+  { id: 3, name: "ITF", choosed: false },
+  { id: 4, name: "澳网", choosed: false },
+  { id: 5, name: "法网", choosed: false },
+  { id: 6, name: "温网", choosed: false },
+  { id: 7, name: "美网", choosed: false },
+]
+
+const players_menu = [
+  { id: 0, name: "A", choosed: true },
+  { id: 1, name: "B", choosed: false },
+  { id: 2, name: "C", choosed: false },
+  { id: 3, name: "D", choosed: false },
+  { id: 4, name: "E", choosed: false },
+  { id: 5, name: "F", choosed: false },
+  { id: 6, name: "G", choosed: false },
+
+  { id: 7, name: "H", choosed: false },
+  { id: 8, name: "I", choosed: false },
+  { id: 9, name: "J", choosed: false },
+  { id: 10, name: "K", choosed: false },
+  { id: 11, name: "L", choosed: false },
+  { id: 12, name: "M", choosed: false },
+  { id: 13, name: "N", choosed: false },
+
+  { id: 14, name: "O", choosed: false },
+  { id: 15, name: "P", choosed: false },
+  { id: 16, name: "Q", choosed: false },
+
+  { id: 17, name: "R", choosed: false },
+  { id: 18, name: "S", choosed: false },
+  { id: 19, name: "T", choosed: false },
+
+  { id: 20, name: "U", choosed: false },
+  { id: 21, name: "V", choosed: false },
+  { id: 22, name: "W", choosed: false },
+
+  { id: 23, name: "X", choosed: false },
+  { id: 24, name: "Y", choosed: false },
+  { id: 25, name: "Z", choosed: false },
+]
+
+module.exports = {
+  news: news_menu,
+  players: players_menu
+};
